37223426e3d77c759c38098efbda154334d53c25
akriti-tan <akritikushwah777@gmail.com>
2024-01-03 00:35:51 +0530

    changes

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///market.db'
+app.config["SECRET_KEY"] = "3c5c9bb69735b9677e917d5d"
 db = SQLAlchemy(app)
-
+bcrypt = Bcrypt(app)
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,9 @@
 from app import app
-from flask import render_template
-from app.models import Item
+from flask import render_template, redirect, url_for, flash
+from app.models import Item, User
+from app.form import RegisterForm
+from app import db
+
 @app.route("/")
 @app.route("/home")
 def home_page():
@@ -11,4 +14,25 @@ def market_page():
     items = Item.query.all()
     return render_template("market.html", items=items)
 
+@app.route("/register", methods=["GET", "POST"])
+def register_page():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        user_to_create = User(
+            username=form.username.data,
+            email_address=form.email_address.data,
+            password_hash=form.password1.data
+        )
+        db.session.add(user_to_create)
+        db.session.commit()
+        return redirect(url_for("market_page"))
+    if form.errors != {}:
+        for err_msg in form.errors.values():
+            flash(f"There was an error while creating an Account: {err_msg}", category="danger")
 
+    return render_template("register.html", form=form)
+
+@app.route("/login", methods=["GET", "POST"])
+def login_page():
+    form = LoginForm()
+    return render_template("login.html")
