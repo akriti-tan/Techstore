6ffc148e08a644353e70298a2bc6ad729496df2f
akriti-tan <akritikushwah777@gmail.com>
2024-01-15 20:34:34 +0530

    jan 15

--- app/__init__.py
+++ app/__init__.py
@@ -11,4 +11,5 @@ db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = "login_page"
+login_manager.login_message_category = "info"
 from app import routes
